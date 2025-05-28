dd673d1ee7dbb24da4a50b99d6da78a8ce18e28f
fabricío araújo em vocacao <mrprince2099@gmail.com>
2025-05-28 16:16:14 -0300

    Add files via upload

--- rock-paper-scissors/assets/js/script.js
+++ rock-paper-scissors/assets/js/script.js
@@ -1,10 +1,12 @@
+// Obter elementos DOM
 const gameContainer = document.querySelector(".container");
 const userResult = document.querySelector(".user_result img");
 const botResult = document.querySelector(".bot_result img");
 const result = document.querySelector(".result");
 const optionImages = document.querySelectorAll(".option_image");
 
-const botImages = ["./assets/images/rock.png", "./assets/images/paper.png", "./assets/images/scissors.png"];
+//Definir imagens e resultado possíveis
+const botImages = ["./assets/rock.png", "./assets/paper.png", "./assets/scissors.png"];
 const outcomes = {
   RR: "Draw",
   RP: "BOT",
@@ -17,11 +19,16 @@ const outcomes = {
   SP: "YOU"
 };
 
+//Manipulador de eventos para clique em imagem
 function handleOptionClick(event) {
   const clickedImage = event.currentTarget;
   const clickedIndex = Array.from(optionImages).indexOf(clickedImage);
 
-  userResult.src = botResult.src = "./assets/rock.png";
+  // Redefine os resultados e exibe "Aguarde..."
+  botResult.src = botResult.src = "./assets/pedra.png";
+  botResult.src = botResult.src = "./assets/papel.png";
+  botResult.src = botResult.src = "./assets/pedra.png";
+  userResult.src = botResult.src = "./assets/pedra.png";
   result.textContent = "Wait...";
 
   optionImages.forEach((image, index) => {
@@ -33,22 +40,25 @@ function handleOptionClick(event) {
   setTimeout(() => {
     gameContainer.classList.remove("start");
 
+     // Definir imagens de usuário e bot 
     const userImageSrc = clickedImage.querySelector("img").src;
     userResult.src = userImageSrc;
 
     const randomNumber = Math.floor(Math.random() * botImages.length);
     const botImageSrc = botImages[randomNumber];
-    botResult.src = botImageSrc;
+    userResult.src = userImageSrc;
 
     const userValue = ["R", "P", "S"][clickedIndex];
     const botValue = ["R", "P", "S"][randomNumber];
     const outcomeKey = userValue + botValue;
     const outcome = outcomes[outcomeKey] || "Unknown";
-
-    result.textContent = userValue === botValue ? "Match Draw" : `${outcome} WiN!`;
+    
+    // Exibe o resultado
+    result.textContent = botValue === userValue ? "Match Draw" : `${outcome} WiN!`;
   }, 2500);
 }
 
+// Anexar ouvintes de eventos ás imagens de opções
 optionImages.forEach(image => {
   image.addEventListener("click", handleOptionClick);
 });
